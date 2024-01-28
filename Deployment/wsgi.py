9e2259e83566916c3a175d62e232829d839b3dc7
Fredrick Kyeki <101971767+FREDRICKKYEKI@users.noreply.github.com>
2024-01-29 01:42:54 +0300

    Update wsgi.py

--- Deployment/wsgi.py
+++ Deployment/wsgi.py
@@ -5,7 +5,7 @@ import os
 sys.path.insert(0, os.path.dirname(__file__))
 
 # Import app
-from .app import app
+from app import app
 
 # Define the app to be used by the WSGI server
 application = app
